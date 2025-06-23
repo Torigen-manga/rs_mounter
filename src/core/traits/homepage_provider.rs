fc5b43c6db8a4f44885b9ef4db0b0c4254eb53d9
YuriVGR <yurivgr@icloud.com>
2025-06-23 12:15:00 -0300

    abc

--- src/core/traits/homepage_provider.rs
+++ src/core/traits/homepage_provider.rs
@@ -1,8 +1,8 @@
-use crate::generated::{PagedResults, Section, SectionEntry};
+use crate::generated::{Homepage, PagedResults, SectionEntry};
 use async_trait::async_trait;
 
 #[async_trait]
 pub trait HomepageProvider {
-    fn get_homepage(&self) -> Option<Vec<Section>>;
+    fn get_homepage(&self) -> Result<Homepage, String>;
     fn get_view_more_items(&self, section_id: &str) -> Result<PagedResults<SectionEntry>, String>;
 }
