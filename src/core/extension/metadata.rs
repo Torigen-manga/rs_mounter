4296e4e3dfb0abbc41b5be213b37c279351e5636
YuriVGR <yurivgr@icloud.com>
2025-06-25 10:21:13 -0300

    ambiguity fix

--- src/core/extension/metadata.rs
+++ src/core/extension/metadata.rs
@@ -2,12 +2,6 @@ use crate::generated::{MetadataModel, MetadataSchemaMap};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct MetadataSchema {
-    pub view_more: HashMap<String, MetadataSchemaMap>,
-    pub search: HashMap<String, MetadataModel>,
-}
-
 pub trait SourceMetadataProvider {
     fn get_metadata(&self) -> MetadataSchema;
 }
